cbddb5948bfe06e2ef4f970878f093644404dd89
hemanth <applicationcomputer257@gmail.com>
2023-06-04 09:57:18 +0530

    mongodb connection

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const {connectToDB,getDb}=require('./db')
 // init app & middleware
 const app=express()
 
-app.listen(3000,()=>{
-console.log(`lisening at port 3000`)
+// db connection 
+
+let db;
+
+connectToDB((err)=>{
+    if(!err){
+app.listen(3000, () => {
+  console.log(`lisening at port 3000`);
+})
+
+db=getDb()
+    }
 })
 
 
